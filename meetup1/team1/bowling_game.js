557b24cb638865ddd1c98d1b5668c89b28584175
Lucian Condrea <lucian@condrea.dev>
2020-10-01 19:27:33 -0400

    mob next [ci-skip]

--- meetup1/team1/bowling_game.js
+++ meetup1/team1/bowling_game.js
@@ -1,9 +1,16 @@
 function bowlingGame(gameString) {
+  let score = 0;
+  const frames = getFrames(gameString);
+  let index = 0;
+  while (index < 10) {
+    console.log(frames[index]);
+    index++;
+  }
   return "apples";
 }
 
 function getFrames(str) {
-  return str.split('|').filter(s =>  s !== "")
+  return str.split("|").filter((s) => s !== "");
 }
 
-module.exports = {bowlingGame, getFrames};
+module.exports = { bowlingGame, getFrames };
