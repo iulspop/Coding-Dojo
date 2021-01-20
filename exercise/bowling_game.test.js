27b6952df8d10993607e44a31d471f438e62f309
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-20 08:41:11 -0500

    add complex test

--- exercise/bowling_game.test.js
+++ exercise/bowling_game.test.js
@@ -12,4 +12,32 @@ describe('bowlingGame', () => {
   it('calculates score for line with strikes', () => {
     expect(bowlingGame("X|X|X|X|X|X|X|X|X|X||XX")).toEqual(300);
   });
+
+  it('calculates score for line with stikes, spares, numbers amd miss', () => {
+    expect(bowlingGame("X|5/|23|8/|45|X|--|-4|-/|2/||5")).toEqual(101);
+  });
 });
+
+// 10
+// 5
+// 5
+
+// 10
+// 2
+
+// 5
+
+// 10
+// 4
+
+// 9
+
+// 10
+
+// 4
+
+// 10
+// 2
+
+// 10
+// 5
